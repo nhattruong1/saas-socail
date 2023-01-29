40917bf240881727ed8a0f9fc7c1931570bac123
Võ Nhật Trường <truongvn@vng.com.vn>
2023-01-29 19:27:38 +0700

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TenantModule } from "./tenant/tenant.module";
 import { MasterModule } from "./master/master.module";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
+  controllers: [AppController],
+  providers: [AppService],
   imports: [TenantModule, MasterModule],
 })
 export class AppModule {}
--- src/master/authentication/authentication.module.ts
+++ src/master/authentication/authentication.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { AuthenticationService } from './authentication.service';
-import { AuthenticationController } from './authentication.controller';
+import { Module } from "@nestjs/common";
+import { AuthenticationService } from "./authentication.service";
+import { AuthenticationController } from "./authentication.controller";
 
 @Module({
   controllers: [AuthenticationController],
-  providers: [AuthenticationService]
+  providers: [AuthenticationService],
 })
 export class AuthenticationModule {}
